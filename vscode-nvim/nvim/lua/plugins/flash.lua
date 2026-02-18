5f5fcefd007cfb5c5d3a4a9e05ee7620966ddc3c
NguyenXuanVinh <nongami87@gmail.com>
2026-02-19 00:01:55 +0700

    feat(vscode-nvim/plugins): add theme-based highlight groups for Flash
    - introduces conditional highlight group configurations based on the active color theme.
    - ensures visual consistency for Flash plugin UI elements when switching between supported themes such as "poimandres" and "Vesper".

--- vscode-nvim/nvim/lua/plugins/flash.lua
+++ vscode-nvim/nvim/lua/plugins/flash.lua
@@ -6,10 +6,21 @@ return {
   config = function(_, opts)
     require("flash").setup(opts);
 
-    vim.api.nvim_set_hl(0, "FlashBackdrop", { fg = "#767c9d" })
-    vim.api.nvim_set_hl(0, "FlashCurrent", { bg = "#ffffff", fg = "#1b1e28" })
-    vim.api.nvim_set_hl(0, "FlashLabel", { bg = "#5de4c7", fg = "#ffffff", bold = true })
-    vim.api.nvim_set_hl(0, "FlashMatch", { bg = "#272c3b", fg = "#ffffff" })
+    local vscode = require('vscode')
+    local current_theme = vscode.get_config("workbench.colorTheme")
+
+    if current_theme == "poimandres" then
+      vim.api.nvim_set_hl(0, "FlashBackdrop", { fg = "#767c9d" })
+      vim.api.nvim_set_hl(0, "FlashCurrent", { bg = "#ffffff", fg = "#1b1e28" })
+      vim.api.nvim_set_hl(0, "FlashLabel", { bg = "#5de4c7", fg = "#ffffff", bold = true })
+      vim.api.nvim_set_hl(0, "FlashMatch", { bg = "#272c3b", fg = "#ffffff" })
+    elseif current_theme == "Vesper" then
+      vim.api.nvim_set_hl(0, "FlashBackdrop", { fg = "#707070" })
+      vim.api.nvim_set_hl(0, "FlashCurrent", { bg = "#ffffff", fg = "#101010" })
+      vim.api.nvim_set_hl(0, "FlashLabel", { bg = "#ffc799", fg = "#101010", bold = true })
+      vim.api.nvim_set_hl(0, "FlashMatch", { bg = "#323232", fg = "#ffffff" })
+    end
+
     vim.api.nvim_set_hl(0, "FlashCursor", { reverse = true })
   end,
   keys = {
