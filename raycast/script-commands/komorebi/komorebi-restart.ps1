286c31b8a20d061cec626e751100af0551e9c4b7
NguyenXuanVinh <nongami87@gmail.com>
2026-03-14 07:34:40 +0700

    refactor(raycast): reorganize script commands into subfolders
    - moves komorebi and yasb script command files into dedicated subdirectories for improved project
    structure and maintainability.
    - updates relative paths to environment files to reflect new locations.

--- raycast/script-commands/komorebi/komorebi-restart.ps1
+++ raycast/script-commands/komorebi/komorebi-restart.ps1
@@ -1,6 +1,6 @@
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Komorebi Start
+# @raycast.title Komorebi Restart
 # @raycast.mode silent
 
 # Optional parameters:
@@ -10,10 +10,11 @@
 # @raycast.author nivx18818
 # @raycast.authorURL https://raycast.com/nivx18818
 
-$envFile = "$PSScriptRoot\..\..\pwsh\env.ps1"
+$envFile = "$PSScriptRoot\..\..\..\pwsh\env.ps1"
 if (-not (Test-Path $envFile)) {
     throw "Required env file missing: $envFile"
 }
 . $envFile
 
+komorebic stop --whkd
 komorebic start --whkd
--- raycast/script-commands/komorebi/komorebi-start.ps1
+++ raycast/script-commands/komorebi/komorebi-start.ps1
@@ -1,20 +1,19 @@
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title YASB Restart
+# @raycast.title Komorebi Start
 # @raycast.mode silent
 
 # Optional parameters:
-# @raycast.icon 🎴
+# @raycast.icon 🍉
 
 # Documentation:
 # @raycast.author nivx18818
 # @raycast.authorURL https://raycast.com/nivx18818
 
-$envFile = "$PSScriptRoot\..\..\pwsh\env.ps1"
+$envFile = "$PSScriptRoot\..\..\..\pwsh\env.ps1"
 if (-not (Test-Path $envFile)) {
     throw "Required env file missing: $envFile"
 }
 . $envFile
 
-yasbc stop
-yasbc start
+komorebic start --whkd
