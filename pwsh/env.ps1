8bbd6029b7257a6d57a1b15dae5bd97f5d1c4e22
NguyenXuanVinh <nongami87@gmail.com>
2026-03-16 19:36:34 +0700

    feat(pwsh): add pipx environment variables
    - configures environment variables for pipx installation, enabling
    easy access to pipx binaries and global directories.
    - improves support for Python package management in user environment.

--- pwsh/env.ps1
+++ pwsh/env.ps1
@@ -8,6 +8,13 @@ $Env:WHKD_CONFIG_HOME="$Env:DOTFILES\komorebi\whkd"
 $Env:YASB_CONFIG_HOME="$Env:DOTFILES\yasb\$Theme"
 $Env:XDG_CONFIG_HOME="$HOME\.config"
 
+$Env:PIPX_HOME="D:\apps\pipx\share\pipx"
+$Env:PIPX_BIN_DIR="D:\apps\pipx\bin"
+$Env:PIPX_MAN_DIR="D:\apps\pipx\share\man"
+$Env:PIPX_GLOBAL_HOME=$Env:PIPX_HOME
+$Env:PIPX_GLOBAL_BIN_DIR=$Env:PIPX_BIN_DIR
+$Env:PIPX_GLOBAL_MAN_DIR=$Env:PIPX_MAN_DIR
+
 if ($Theme -eq "catppuccin") {
     $OmpTheme = "poimandres"
     $InlinePrediction = "#767c9d"
