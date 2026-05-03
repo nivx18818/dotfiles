d4094b9fdf38e76196fd6dec9be8d29950e96a53
NguyenXuanVinh <nongami87@gmail.com>
2026-05-03 08:27:41 +0700

    refactor(nvim): update buffer keys, show all in file tree
    - removes old buffer navigation keymaps in favor of alternatives
    and configures the file explorer to display dotfiles, hidden,
    and gitignored files, while always hiding the .git folder.

--- nvim/lua/nivx/plugins/ui/neo-tree.lua
+++ nvim/lua/nivx/plugins/ui/neo-tree.lua
@@ -41,6 +41,15 @@ return {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+        never_show = {
+          ".git",
+        }
+      }
     },
 
     window = {
